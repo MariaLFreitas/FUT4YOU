d6e422b9bc9878f2d0a4e2db9e2048f2a627886f
luizaa_sl <luiza.stringhini@gmail.com>
2021-08-24 15:10:31 -0300

    tela home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,35 @@
+//imagem
+import fotocampo from '../assets/images/fotohome.jpg';
+import iconepessoa from '../assets/images/people.svg';
+
+//componentes
+import { Button } from '../Components/Button';
+
+//style da home
+import '../styles/home.css';
+
 export function Home(){
     return(
-        <div>
-            <h1>HOME</h1>
+        <div id="home">
+            <header>
+                <p id="logo">FUT4YOU</p>
+                <span className="navbartext">Meu time</span>
+                <span className="navbartext">Calendário de jogos</span>
+                <span className="navbartext">Comunidade</span>
+                { <img id="icone" src={iconepessoa} alt="usuario" /> }
+            </header>
+            <main>
+                  <img id="imageFotoHome" src={fotocampo} alt=" imagem de bola no campo" /> 
+                               
+                <div id="titulo">
+                    <span>VOCÊ COMEÇA O JOGO!</span>
+                
+                </div>
+                <div id="subtitulo">
+                    <p>Venha fazer parte da maior comunidade de jogos de futebol do Brasil!</p>
+                </div>
+                <Button id="botaohome" type="submit">Começar agora</Button>
+            </main>
         </div>
     )
-}
+} 
